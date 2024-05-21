bd3dcb400a825ec404a59091012f12875c129c35
flowmellow <michael.floresmmt@gmail.com>
2024-05-21 09:12:31 -0400

    Add project files.

---
+++ Assignment_3.1.5/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Write a function which takes an array as input and finds the first occurrence of 2 consecutive 1s and changes their value to 0.
+For e.g: Input : [0,2,1,1,9,1,1]
+Output: [0,2,0,0,9,1,1]
+*/
+
+namespace Assignment_3._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] originalArray= {0, 2, 1, 1, 9, 1, 1 };
+            int targetNum = 1;
+
+            Console.Write("Input: ");
+            foreach (int i in originalArray)
+            {
+                
+                Console.Write(i);
+            }
+           
+
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                if (originalArray[i] == targetNum && originalArray[i + 1] == targetNum)
+                {
+                    originalArray[i] = 0;
+                    originalArray[i + 1] = 0;
+                    break;
+                }
+              
+            }
+            Console.WriteLine();
+            Console.Write("Output: ");
+            foreach (int i in originalArray)
+            {                
+                Console.Write(i);
+            }
+        }
+
+    
+    }
+    
+}
